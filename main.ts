821119ad934575e23e9f3b19f85c881faf290a50
KiranRiaz57 <159231693+KiranRiaz57@users.noreply.github.com>
2024-03-14 22:23:35 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,29 @@
+// Q31
+// let username: string[] = [];
+// if(username.length === 0){
+//     console.log("we need to find some users!")
+//     }
+// Q32
+// let current_users: string[] = ["user1","admin","user3","user4","user5"];
+// let new_users: string[] = ["user1","user6","user7","admin","user9"]
+// new_users.forEach(newUser=>{
+//     if(current_users.some(currentUser =>currentUser.toLowerCase()===newUser.toLowerCase()))
+// {
+//     console.log(`${newUser} will need to enter a new username.`)
+// }else{
+//     console.log(`${newUser} is available.`)
+// }})
+//Q33
+// let numbers: number[] = [1,2,3,4,5,6,7,8,9];
+// numbers.forEach(number =>{
+//     let suffix = "th"
+//     if(number === 1){
+//         suffix = "st"
+//     }else if(number === 2){
+//         suffix = "nd"
+//     }else if (number === 3){
+//         suffix ="rd"
+//     }{
+//         console.log(`${number} ${suffix}`)
+//     }
+// })
